9a780aa64c992cd8218f6f6e68852fb58585c4a2
Niels Braczek <nbraczek@bsds.de>
2015-07-20 14:00:40 +0200

    Removed version chack - this belongs to the installer

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
  */
 
 /**
- * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
- */
-define('JOOMLA_MINIMUM_PHP', '5.3.10');
-
-if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
-{
-	die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
-}
-
-/**
  * Constant that is checked in included files to prevent direct access.
  * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
  */
