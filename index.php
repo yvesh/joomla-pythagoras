d066ad7e06a81733f620d766d08b0f7f5c1fd587
Niels Braczek <nbraczek@bsds.de>
2015-07-20 14:01:33 +0200

    Removed includes - thay are app specific and/or resolved by the autoloader

--- index.php
+++ index.php
@@ -12,22 +12,6 @@
  */
 define('_JEXEC', 1);
 
-if (file_exists(__DIR__ . '/defines.php'))
-{
-	include_once __DIR__ . '/defines.php';
-}
-
-if (!defined('_JDEFINES'))
-{
-	define('JPATH_BASE', __DIR__);
-	require_once JPATH_BASE . '/includes/defines.php';
-}
-
-require_once JPATH_BASE . '/includes/framework.php';
-
-// Mark afterLoad in the profiler.
-JDEBUG ? $_PROFILER->mark('afterLoad') : null;
-
 // Instantiate the application.
 $app = JFactory::getApplication('site');
 
